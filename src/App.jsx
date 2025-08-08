cd7f14335f69eedc6966006a90047c2940643089
jeyoung <jdngh@naver.com>
2025-08-08 17:44:14 +0900

    Feat : router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
