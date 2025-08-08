bce5474508fccb5a5114da4b6ad4a346be865b4f
jeyoung <jdngh@naver.com>
2025-08-08 17:54:16 +0900

    Feat : Route 페이지 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainAnser from '../src/pages/MainAnswer';
 import './App.css';
+import AnswerQuestion from './pages/AnserQuestion';
+import Answer from './pages/Answer';
+import AnswerList from './pages/AnswerList';
+import AnswerModal from './pages/AnswerModal';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<MainAnser />} />
+        <Route path="/list" element={<AnswerList />} />
+        <Route path="/list/1" element={<AnswerQuestion/>} />
+        <Route path="/list/1/answer" element={<Answer />} />
+        <Route path="/modal" element={<AnswerModal />} />
       </Routes>
     </BrowserRouter>
   );
