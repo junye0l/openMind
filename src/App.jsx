00af7c45d0d6b767ec6151fb979b22389b006c4d
정찬민 <setcorin@naver.com>
2025-08-12 22:51:51 +0900

    dev pull 후 찬민 브랜치로 머지

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <p className="font-bold text-[80px]">Hello, gitmoji test</p>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
