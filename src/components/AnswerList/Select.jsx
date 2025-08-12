fbab3204089fb89de6e9f7727733ffea7a0f2311
jeyoung <jdngh@naver.com>
2025-08-12 19:01:40 +0900

    📝 Doc : getsubjects BASEURL 적용, pointer 오타 수정

--- src/components/AnswerList/Select.jsx
+++ src/components/AnswerList/Select.jsx
@@ -6,7 +6,7 @@ export default function Select({ handleNew, handleName }) {
   const [selectValue, setSelectValue] = useState('이름순');
   const options = ['이름순', '최신순'];
 
-  const baseClass = `${'py-2 px-3 text-[14px] text-left cursor-pointe w-[100%]'}`;
+  const baseClass = `${'py-2 px-3 text-[14px] text-left cursor-pointer w-[100%]'}`;
   const activeClass = `${'text-[#1877F2]'}`;
   const changeIcon = `${'rotate-[-180deg]'}`;
   const baseIcon = `${`size-[14px] transition duration-300`}`;
@@ -38,7 +38,7 @@ export default function Select({ handleNew, handleName }) {
         />
       </button>
       {isOpen && (
-        <div className="border rounded-lg mt-1 absolute w-[79px] shadow-[#8C8C8C40] shadow-lg z-10 cursor-pointe bg-white">
+        <div className="border rounded-lg mt-1 absolute w-[79px] shadow-[#8C8C8C40] shadow-lg z-10 cursor-pointer bg-white">
           {options.map(item => {
             return (
               <button
--- src/hooks/getsubjects.js
+++ src/hooks/getsubjects.js
@@ -4,7 +4,7 @@ export async function getsubjects({ limit, page, team = '18-1' }) {
   try {
     const offset = (page - 1) * limit;
     const response = await fetch(
-      `https://openmind-api.vercel.app/${team}/subjects/?limit=${limit}&offset=${offset}`
+      `${BASEURL}/${team}/subjects/?limit=${limit}&offset=${offset}`
     );
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
