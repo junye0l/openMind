5891d094494809be8ca25b3bd28b4d54c041fb80
jeschun <jeschun54@gmail.com>
2025-08-18 23:00:40 +0900

    ♻️  refactor: FloatingButton 클릭 시 open-question-modal 이벤트 발생하도록 수정

--- src/components/question/FloatingButton.jsx
+++ src/components/question/FloatingButton.jsx
@@ -1,7 +1,12 @@
 function FloatingButton() {
   return (
     <div className="fixed bottom-[24px] right-[24px]">
-      <button className="px-[49.5px] py-[14.5px] bg-bn-40 text-gs-10 shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-full">
+      <button
+        className="px-[49.5px] py-[14.5px] bg-bn-40 text-gs-10 shadow-[0_4px_4px_rgba(0,0,0,0.25)] rounded-full"
+        onClick={() =>
+          window.dispatchEvent(new CustomEvent('open-question-modal'))
+        }
+      >
         질문 작성하기
       </button>
     </div>
