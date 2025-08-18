f3e2c051f69f24c4b6d6136dd4de5874c269614a
정찬민 <setcorin@naver.com>
2025-08-18 18:35:14 +0900

    💄 1차 pr 피드백 수정 및 svgr컴포넌트, 수정하기 ui 작성
    1차 pr 피드백 수정 및 svgr컴포넌트, 수정하기 ui 작성

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,17 @@ import AnswerQuestion from './pages/AnserQuestion';
 import Answer from './pages/Answer';
 import AnswerList from './pages/AnswerList';
 import AnswerModal from './pages/AnswerModal';
-import Answerpage from './components/Answerpage/Answerpage.jsx';
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<MainAnswer />} />
         <Route path="/list" element={<AnswerList />} />
         <Route path="/subjects/:id" element={<AnswerQuestion />} />
         <Route path="/post/:id/answer" element={<Answer />} />
         <Route path="/modal" element={<AnswerModal />} />
-      </Routes> */}
-      <Answerpage />;
+      </Routes>
     </BrowserRouter>
   );
 }
