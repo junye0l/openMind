1e1fd0bf7dc4c0dae0645b776e558697984fbb9b
FE-kimwonseon <gws8221@gmail.com>
2025-08-22 18:28:36 +0900

    ♻️  style : 반응현 코드 재설정

--- src/components/question/ProfileSection.jsx
+++ src/components/question/ProfileSection.jsx
@@ -6,15 +6,15 @@ import CopyUrlButton from './ShareButton/CopyUrlButton';
 
 function ProfileSection({ userInfo }) {
   return (
-    <section className="absolute top-[50px] mobile:top-[40px] flex justify-center flex-col items-center gap-[12px]">
+    <section className="absolute top-[50px] max-sm:top-[40px] flex justify-center flex-col items-center gap-[12px]">
       <Link to={'/list'}>
-        <Logo alt="로고 이미지" className="mobile:w-[124px] mobile:h-[49px]" />
+        <Logo alt="로고 이미지" className="max-sm:w-[124px] max-sm:h-[49px]" />
       </Link>
       <img
-        className="w-[136px] h-[136px] mobile:w-[104px] mobile:h-[104px] rounded-full"
+        className="w-[136px] h-[136px] max-sm:w-[104px] max-sm:h-[104px] rounded-full"
         src={userInfo.imageSource}
       />
-      <p className="text-[32px] mobile:text-[24px]">{userInfo.name}</p>
+      <p className="text-[32px] max-sm:text-[24px]">{userInfo.name}</p>
       <div className="flex gap-[12px] justify-center">
         <CopyUrlButton />
         <KakaoShareButton userInfo={userInfo} />
