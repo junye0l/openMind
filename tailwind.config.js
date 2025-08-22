7ed6a056b4aa2933278cdfd8a2178550b03ffcbd
FE-kimwonseon <gws8221@gmail.com>
2025-08-22 14:22:07 +0900

    📝  doce : TailWindCSS 반응형 커스텀 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      mobile: { max: '375px' },
+      tablet: { max: '768px' },
+      desktop: { max: '1200px' },
+    },
     extend: {
       colors: {
         gs: {
