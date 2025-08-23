6b66cd8c29474647bc36e6d3637abd0de223759b
junye0l <junyeolkim00@gmail.com>
2025-08-23 19:21:50 +0900

    🚑️  fix : 컴포넌트 주소 수정

--- src/pages/MainAnswer.jsx
+++ src/pages/MainAnswer.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import MainComponents from '../components/MainPage/MainComponents';
+import MainComponents from '../components/mainpage/MainComponents';
 export default function 메인페이지() {
   return (
     <>
